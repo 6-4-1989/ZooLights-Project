83511493314463ee8c75a2f2608899527572213d
enquista <enquista@wlhs.wlwv.k12.or.us>
2024-12-17 14:18:25 -0800

    drivethrough now with comments

--- src/driveThrough.java
+++ src/driveThrough.java
@@ -1,13 +1,13 @@
 public class driveThrough {
     public static void main(String[] args)
     {
-        double price = 65;
-        if(peopleCount > 8) {
+        double price = 65; //sets initial price for drive through
+        if(peopleCount > 8) { //adds price for >8 people
             price += (peopleCount-8) * 12;
         }
-        if(discountCode.equals("MEMBER")) {
+        if(discountCode.equals("MEMBER")) { //discount code application
             price *= 0.8;
         }
-        System.out.println("Total Price: $" + price);
+        System.out.println("Total Price: $" + price); //output
     }
 }
