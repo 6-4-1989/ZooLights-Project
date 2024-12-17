b8de761e16ce752d9a9bf3ecc6f17f33a29ce543
link_wlhs <kelin19772@gmail.com>
2024-12-17 14:40:01 -0800

    David Johnson

--- src/walkThrough.java
+++ src/walkThrough.java
@@ -1,2 +1,23 @@
-public class walkThrough {
+import java.util.Random;
+
+public class walkThrough
+{
+
+    public static String transportationModes(int filler, int fillerStuff, String fillerTicket)
+    {
+        Random random = new Random();
+
+        switch (filler)
+        {
+            case 1 -> fillerTicket = "David";
+            //fill Driver info here
+            case 2 -> {
+                for (int i = 0; i < 5; i++) {
+                    fillerStuff = random.nextInt(10);
+                    fillerTicket += String.valueOf(fillerStuff);
+                }
+            }
+        }
+        return fillerTicket;
+    }
 }
