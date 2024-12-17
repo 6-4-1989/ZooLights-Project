772dabaaaf63562bff177afe6d6cebc888bbd409
tristan <tdbrester@gmail.com>
2024-12-17 14:32:02 -0800

    make main

--- src/main.java
+++ src/main.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
 public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //get all info
+        System.out.print("Enter the amount of people in the party >");
+        int people = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter your first and last name >");
+        String name = sc.nextLine();
+
+        System.out.print("Enter the date you want to attend (mm/dd/yyyy)>");
+        String date = sc.nextLine();
+
+        System.out.print("Enter your discount code >");
+        String discount = sc.nextLine();
+
+        System.out.print("Do you want to do drive through or walk through (drive/walk)? >");
+        String transportation = sc.nextLine();
+        if(transportation.equalsIgnoreCase("walk")) {
+            System.out.print("Do you wish to ride the train (yes/no)? >");
+            String ride = sc.nextLine();
+            if (ride.equalsIgnoreCase("yes")) {
+                System.out.print("Enter your height (in inches) >");
+                double height = sc.nextDouble();
+                sc.nextLine();
+
+                System.out.print("Enter you weight (in lbs) >");
+                double weight = sc.nextDouble();
+                sc.nextLine();
+            }
+        }
+
+        System.out.print("Enter your date of birth (mm/dd/yyyy) >");
+        String dateOfBirth = sc.nextLine();
+
+    }
 }
