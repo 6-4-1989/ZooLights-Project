a572b5c9bf9c421fa736a506d2bae1b4751f3ee1
enquista <enquista@wlhs.wlwv.k12.or.us>
2024-12-17 14:16:56 -0800

    drivethrough

--- src/driveThrough.java
+++ src/driveThrough.java
@@ -1,2 +1,13 @@
 public class driveThrough {
+    public static void main(String[] args)
+    {
+        double price = 65;
+        if(peopleCount > 8) {
+            price += (peopleCount-8) * 12;
+        }
+        if(discountCode.equals("MEMBER")) {
+            price *= 0.8;
+        }
+        System.out.println("Total Price: $" + price);
+    }
 }
